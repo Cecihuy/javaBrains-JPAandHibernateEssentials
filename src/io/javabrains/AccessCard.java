d8477f5369476af8b904d92910b31b24de0068ae
sourceTree <sourcetree@mail.com>
2024-01-19 22:31:25 -0700

    23 Creating the second entity

---
+++ src/io/javabrains/AccessCard.java
@@ -0,0 +1,46 @@
+package io.javabrains;
+import java.util.Date;
+
+import org.hibernate.annotations.Type;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AccessCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int id;
+    private Date issuedDate;
+    private boolean isActive;
+    private String firmwareVersion;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getIssuedDate() {
+        return issuedDate;
+    }
+    public void setIssuedDate(Date issuedDate) {
+        this.issuedDate = issuedDate;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+    public void setFirmwareVersion(String firmwareVersion) {
+        this.firmwareVersion = firmwareVersion;
+    }
+}
--- src/io/javabrains/JpaStarterMain.java
+++ src/io/javabrains/JpaStarterMain.java
@@ -1,4 +1,5 @@
 package io.javabrains;
+import java.util.Date;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -6,15 +7,39 @@ import jakarta.persistence.Persistence;
 
 public class JpaStarterMain{
     public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("Foo Bar");
+        employee.setSsn("123");
+        employee.setDob(new Date());
+        employee.setAge(20);
+        employee.setType(EmployeeType.CONTRACTOR);
+
+        Employee employee2 = new Employee();
+        employee2.setName("Bar Baz");
+        employee2.setSsn("1234");
+        employee2.setDob(new Date());
+        employee2.setAge(30);
+        employee2.setType(EmployeeType.FULL_TIME);
+
+        AccessCard card = new AccessCard();
+        card.setIssuedDate(new Date());
+        card.setActive(true);
+        card.setFirmwareVersion("1.0.0");
+
+        AccessCard card2 = new AccessCard();
+        card2.setIssuedDate(new Date());
+        card2.setActive(false);
+        card2.setFirmwareVersion("1.2.0");
         
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("myApp");
-        EntityManager entityManager = factory.createEntityManager();
-        
-        Employee employee = entityManager.find(Employee.class, 1);
-
+        EntityManager entityManager = factory.createEntityManager();        
         EntityTransaction transaction = entityManager.getTransaction();
+        
         transaction.begin();
-        entityManager.remove(employee);
+        entityManager.persist(employee);
+        entityManager.persist(employee2);
+        entityManager.persist(card);
+        entityManager.persist(card2);
         transaction.commit();
         entityManager.close();
         factory.close();
