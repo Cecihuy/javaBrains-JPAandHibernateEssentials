783029320f3b1e394aec688eba055db27104388b
sourceTree <sourcetree@mail.com>
2024-01-19 21:42:51 -0700

    21 Update operation with JPA

--- src/io/javabrains/JpaStarterMain.java
+++ src/io/javabrains/JpaStarterMain.java
@@ -1,17 +1,24 @@
 package io.javabrains;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class JpaStarterMain{
     public static void main(String[] args) {
         
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("myApp");
+        EntityManager entityManager = factory.createEntityManager();
+        
+        Employee employee = entityManager.find(Employee.class, 1);        
+        employee.setAge(30);
+        employee.setType(EmployeeType.FULL_TIME);
 
-        Employee employee = entityManager.find(Employee.class, 1);
-        Employee employee2 = entityManager.find(Employee.class, 2);
-        System.out.println(employee);
-        System.out.println(employee2);
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(employee);
+        transaction.commit();
+        entityManager.close();
+        factory.close();
     }
 }
