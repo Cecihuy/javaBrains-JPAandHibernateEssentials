c6800cb5fcd164e52681204bb12635af3ba33d1c
sourceTree <sourcetree@mail.com>
2024-01-23 20:45:03 -0700

    51 Declarative Transactions

--- springbootjpaexample/src/main/java/io/javabrains/springbootjpaexample/SpringbootjpaexampleApplication.java
+++ springbootjpaexample/src/main/java/io/javabrains/springbootjpaexample/SpringbootjpaexampleApplication.java
@@ -4,10 +4,14 @@ import io.javabrains.springbootjpaexample.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootjpaexampleApplication {
 	@Autowired
 	EmployeeRepository employeeRepository;
@@ -19,6 +23,12 @@ public class SpringbootjpaexampleApplication {
 		Optional<Employee> employee = employeeRepository.findById(1);
 		if(employee.isPresent()){
 			System.out.println(employee.get());
+			updateEmployee(employee.get());
 		}
+	}
+	@Transactional(rollbackOn = SQLException.class)
+	private void updateEmployee(Employee employee) {
+		employee.setName("Updated name");
+		employeeRepository.save(employee);
 	}
 }
